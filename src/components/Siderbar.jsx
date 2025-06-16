d6495bc627a510d04b78c316dde6594705dbfe54
Jack Kong <jackadoy157@gmail.com>
2025-06-16 18:36:34 +1000

    Implemented overlay sidebar and transition animation

--- src/components/Siderbar.jsx
+++ src/components/Siderbar.jsx
@@ -6,29 +6,36 @@ const navItems = [
         { icon: "src/assets/settings-icon.png", iconSelected: "src/assets/settings-icon-selected.png", label: "Settings", href: "#" },
     ];
 
-export default function NavBar() {
+export default function Sidebar({selectedNav}) {
     const [expanded, setExpanded] = useState(false);
-    const [selectedNav, setSelectedNav] = useState("Dashboard");
-
     const setNavState = () => {
         setExpanded(prev => !prev);
         console.log(expanded);
     }
     return (
-     <nav className={`flex flex-col px-3 py-1 gap-2 bg-white min-h-full shadow-sm ${expanded ? 'w-80' : 'items-center' }`}>
+     <nav className={`fixed top-0 left-0 bottom-0 z-50 flex flex-col px-3 py-1 gap-2 bg-white shadow-sm transition-[width] duration-200 ${expanded ? 'w-80 px-5' : 'w-16 px-2 items-center'}`}>
         <div className="flex flex-row">
-            {expanded ? <img className="w-28" src="src/assets/bb-logo-text.png" alt="Budget buddy logo with text"/> :
+            {expanded ? <img className="h-14" src="src/assets/bb-logo-text.png" alt="Budget buddy logo with text"/> :
                         <img className="w-10" src="src/assets/bb_logo.png" alt="Budget buddy logo"/>
             }
-            <button onClick={setNavState} className={`bg-white px-0.5 py-1 rounded-md border border-gray-400 shadow-sm absolute mt-2 ml-12 ${expanded && 'ml-60 3xl:ml-64'} hover:bg-slate-200 outline-none`}>
-                <img className={`${expanded || 'scale-x-[-1]'}`} src="src/assets/arrow.png" alt="arrow head icon" width="17" />
+            <button
+                onClick={setNavState}
+                className={`absolute top-2 right-[-14px] w-6 h-10 opacity-100 bg-white rounded-md border border-gray-400 shadow-sm hover:bg-slate-200 outline-none flex items-center justify-center`}
+                >
+                <img
+                    className={`${expanded ? '' : 'scale-x-[-1]'}`}
+                    src="src/assets/arrow.png"
+                    alt="Toggle sidebar"
+                />
             </button>
         </div>
         <hr className="h-0.5 w-full rounded-sm bg-gray-400 mt-3 mb-2"/>
 
         {navItems.map(({icon, iconSelected, label, href}) => (
-               <a href={href} className={`${selectedNav === label ? 'bg-primary_blue hover:bg-btn_hover' : 'bg-white hover:bg-slate-200'} rounded-md flex flex-row items-center`}>
-                <img src={selectedNav === label ? iconSelected : icon} alt={`${label} icon`} />
+               <a href={href} className={`${selectedNav === label ? 'bg-primary_blue hover:bg-btn_hover' : 'bg-white hover:bg-slate-200'} 
+                                        rounded-md flex flex-row items-center 
+                                        ${expanded ? 'w-full' : 'w-14'}`}>
+                <img src={selectedNav == label ? iconSelected : icon} alt={`${label} icon`}/>
                 {expanded && (
                     <h1 className={`font-body text-md ${selectedNav === label && 'text-white'}`}>
                         {label}
@@ -37,7 +44,7 @@ export default function NavBar() {
             </a>         
         ))}
         <hr className="h-0.5 rounded-sm w-full bg-gray-400 mt-3 mb-2"/>
-        <a href="#" className="hover:bg-slate-200 rounded-md flex flex-row items-center">
+        <a href="#" className="hover:bg-slate-200 rounded-md flex flex-row items-center w-14">
             <img src="src/assets/logout.png" alt="logout icon" />
             {expanded && 
                 <h1 className={`font-body text-xl text-gray-700 font-bold`}>
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,9 +2,9 @@ import Sidebar from "../../components/Siderbar"
 export default function Dashboard () {
    
    return (
-      <div className="flex flex-row min-h-screen min-w-screen bg-bb_slate">
-        <Sidebar/>
-        <div className="flex flex-col px-5 pb-4 w-full gap-3">
+      <div className="relative min-h-screen min-w-screen bg-bb_slate pl-16">
+        <Sidebar selectedNav="Dashboard"/>
+        <div className="flex flex-col px-5 pb-4 w-full h-full gap-3">
             <h2 className={`text-2xl font-header font-bold ml-4 mt-3`}>
                 <span className="text-primary_blue">Budget</span>
                 <span className="text-secondary_red">Buddy</span>
