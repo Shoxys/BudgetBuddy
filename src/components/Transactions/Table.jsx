acc78f649a142e0461469baca51572c0e838b7e2
Jack Kong <jackadoy157@gmail.com>
2025-06-20 00:03:19 +1000

    Design Transactions layout and implement dynamic data table

--- src/components/Transactions/Table.jsx
+++ src/components/Transactions/Table.jsx
@@ -1,43 +1,93 @@
 import Tag from "../tag";
 import { formatMoney, formatDate } from '../../Utils/helpers';
+import { useState } from "react";
 
 const transactions = [
-  { date: "2025-04-17", amount: -68.96, accountNumber: "899800000", type: "EFTPOS", desc: "POS 17/04", balance: 89.21, category: "Personal Care", merchant: "Beauty Amora" },
-  { date: "2025-04-17", amount: 0, accountNumber: "899800000", type: "EFTPOS", desc: "POS 17/04", balance: 158.17, category: "Uncategorised", merchant: "" },
-  { date: "2025-04-17", amount: -30, accountNumber: "899800000", type: "EFTPOS", desc: "POS 17/04", balance: 158.17, category: "Personal", merchant: "Sephora" },
-  { date: "2025-04-16", amount: -455, accountNumber: "899800000", type: "TRANSFER", desc: "Queensland Electricity", balance: 188.17, category: "Transfers out", merchant: "" },
-  { date: "2025-04-16", amount: 500, accountNumber: "899800000", type: "TRANSFER", desc: "ONLINE DEPOSIT", balance: 643.17, category: "Internal transfers", merchant: "" },
-  { date: "2025-04-15", amount: -12.5, accountNumber: "899800000", type: "MISC", desc: "V3344 10/04", balance: 143.17, category: "Restaurants", merchant: "McDonald's (Waterford West)" },
-  { date: "2025-04-15", amount: -50.44, accountNumber: "899800000", type: "MISC", desc: "V3344 05/04", balance: -168.27, category: "Fuel", merchant: "Shell Coles Express (Marsden)" },
-  { date: "2025-04-14", amount: -25.38, accountNumber: "899800000", type: "MISC", desc: "V3344 05/04", balance: 218.71, category: "Attractions & Events", merchant: "On Air Singing Booth" },
-  { date: "2025-04-13", amount: 12, accountNumber: "899800000", type: "TRANSFER", desc: "Shane N", balance: 244.09, category: "Transfers in", merchant: "" },
-  { date: "2025-04-07", amount: 7.5, accountNumber: "899800000", type: "TRANSFER", desc: "MONFORT", balance: 232.09, category: "Transfers in", merchant: "" },
-  { date: "2025-04-07", amount: 8, accountNumber: "899800000", type: "TRANSFER", desc: "JIMMYMONEY", balance: 224.59, category: "Transfers in", merchant: "" },
-  { date: "2025-04-06", amount: 120, accountNumber: "899800000", type: "TRANSFER", desc: "ONLINE FEE RETURN", balance: 216.59, category: "Internal transfers", merchant: "" },
-  { date: "2025-04-05", amount: 150, accountNumber: "899800000", type: "TRANSFER", desc: "ONLINE GIFT", balance: 265.28, category: "Internal transfers", merchant: "" },
-  { date: "2025-04-01", amount: -10.69, accountNumber: "899800000", type: "MISC", desc: "V3344 30/03", balance: 96.59, category: "Medical", merchant: "Chemist Warehouse (Crestmead)"}
+  { id: 1, date: "2025-04-17", amount: -68.96, accountNumber: "899800000", type: "EFTPOS", desc: "POS 17/04", balance: 89.21, category: "Personal Care", merchant: "Beauty Amora" },
+  { id: 2, date: "2025-04-17", amount: 0, accountNumber: "899800000", type: "EFTPOS", desc: "POS 17/04", balance: 158.17, category: "Uncategorised", merchant: "" },
+  { id: 3, date: "2025-04-17", amount: -30, accountNumber: "899800000", type: "EFTPOS", desc: "POS 17/04", balance: 158.17, category: "Personal", merchant: "Sephora" },
+  { id: 4, date: "2025-04-16", amount: -455, accountNumber: "899800000", type: "TRANSFER", desc: "Queensland Electricity", balance: 188.17, category: "Transfers out", merchant: "" },
+  { id: 5, date: "2025-04-16", amount: 500, accountNumber: "899800000", type: "TRANSFER", desc: "ONLINE DEPOSIT", balance: 643.17, category: "Internal transfers", merchant: "" },
+  { id: 6, date: "2025-04-15", amount: -12.5, accountNumber: "899800000", type: "MISC", desc: "V3344 10/04", balance: 143.17, category: "Restaurants", merchant: "McDonald's (Waterford West)" },
+  { id: 7, date: "2025-04-15", amount: -50.44, accountNumber: "899800000", type: "MISC", desc: "V3344 05/04", balance: -168.27, category: "Fuel", merchant: "Shell Coles Express (Marsden)" },
+  { id: 8, date: "2025-04-14", amount: -25.38, accountNumber: "899800000", type: "MISC", desc: "V3344 05/04", balance: 218.71, category: "Attractions & Events", merchant: "On Air Singing Booth" },
+  { id: 9, date: "2025-04-13", amount: 12, accountNumber: "899800000", type: "TRANSFER", desc: "Shane N", balance: 244.09, category: "Transfers in", merchant: "" },
+  { id: 10, date: "2025-04-07", amount: 7.5, accountNumber: "899800000", type: "TRANSFER", desc: "MONFORT", balance: 232.09, category: "Transfers in", merchant: "" },
+  { id: 11, date: "2025-04-07", amount: 8, accountNumber: "899800000", type: "TRANSFER", desc: "JIMMYMONEY", balance: 224.59, category: "Transfers in", merchant: "" },
+  { id: 12, date: "2025-04-06", amount: 120, accountNumber: "899800000", type: "TRANSFER", desc: "ONLINE FEE RETURN", balance: 216.59, category: "Internal transfers", merchant: "" },
+  { id: 13, date: "2025-04-05", amount: 150, accountNumber: "899800000", type: "TRANSFER", desc: "ONLINE GIFT", balance: 265.28, category: "Internal transfers", merchant: "" },
+  { id: 14, date: "2025-04-01", amount: -10.69, accountNumber: "899800000", type: "MISC", desc: "V3344 30/03", balance: 96.59, category: "Medical", merchant: "Chemist Warehouse (Crestmead)" }
 ];
 
 export default function Table() {
+    const [selectedIds, setSelectedIds] = useState([]);
+    const [lastCheckedIndex, setLastCheckedIndex] = useState(null);
+
+    const handleSelect = (event, id, index) => {
+        if (event.shiftKey && lastCheckedIndex !== null) {
+            const start = Math.min(index, lastCheckedIndex);
+            const end = Math.max(index, lastCheckedIndex);
+
+            const rangeIds = transactions.slice(start, end + 1).map((transaction) => transaction.id);
+            const isDeselecting = selectedIds.includes(id)
+            
+            if (isDeselecting){
+                setSelectedIds((prev) => prev.filter((id) => !rangeIds.includes(id)));
+            } else {
+                 setSelectedIds((prev) => {
+                    const merged = new Set([...prev, ...rangeIds]);
+                    return Array.from(merged);
+                });
+            }       
+        } else {
+            setSelectedIds((prev) =>
+                prev.includes(id)
+                    ? prev.filter((selectedId) => selectedId !== id)
+                    : [...prev, id]
+            );
+        }
+        console.log({ id, index, shift: event.shiftKey, lastCheckedIndex });
+        setLastCheckedIndex(index)
+    };
+
+    const handleSelectAll = () => {
+        if (selectedIds.length === transactions.length) {
+            setSelectedIds([]);
+        } else {
+            setSelectedIds(transactions.map((transaction) => (transaction.id)));
+        }
+    }
+
     return(
-         <div>
             <table className="table-fixed w-full">
                 <thead>
                     <tr className="font-header font-semibold text-md text-gray-800 ">
-                        <th className="w-0"></th>
-                        <th className="w-8">Date</th>
+                        <th className="w-2 text-center py-2">
+                            <input
+                            className="h-5 w-5 outline-none"
+                            name="selectAll"
+                            type="checkbox"
+                            onChange={() => handleSelectAll()}
+                            checked={selectedIds.length === transactions.length}
+                            />
+                        </th>
+                        <th className="w-5 text-center">Date</th>
                         <th className="w-16">Details</th>
-                        <th className="w-7 text-left">Category</th>
+                        <th className="w-7">Category</th>
                         <th className="w-3">Debit</th>
                         <th className="w-3">Credit</th>
                         <th className="w-5">Balance</th>
                     </tr>
                 </thead>
                 <tbody className="">
-                    {transactions.map(({date, amount, type, desc, balance, category, merchant}, index) =>(
-                        <tr key={index} className="border-t border-b border-gray-200 text-md text-gray-600 font-body font-semibold">
-                            <td>
-                                <input className="h-8" type="checkbox" id={index} name={index}></input>
+                    {transactions.map(({id, date, amount, type, desc, balance, category, merchant}, index) =>(
+                        <tr key={id} className={`border-t border-b ${selectedIds.includes(id) ? 'bg-[#008CFF66] opacity-85 border-blue-400' : 'border-gray-200'} text-md text-gray-700 font-body font-semibold`}>
+                            <td className="text-center">
+                                <input className="h-4 w-4 outline-none" 
+                                       type="checkbox" 
+                                       onClick={(event) => handleSelect(event, id, index)} 
+                                       checked={selectedIds.includes(id)}>
+                                </input>
                             </td>
                             <td className="py-3.5 px-2 text-center">{formatDate(date)}</td>
                             <td className="break-words whitespace-normal max-w-[250px]">{merchant === "" ? desc + " " + type : merchant}</td>
@@ -66,6 +116,5 @@ export default function Table() {
                     ))}
                 </tbody>
             </table>
-        </div>  
     )
 }
--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -4,7 +4,7 @@ export default function Transactions() {
    return (
       <div className="relative min-h-screen min-w-screen bg-bb_slate pl-20">
         <Sidebar selectedNav="Dashboard"/>
-        <div className="flex flex-col pb-4 w-full h-full gap-4">
+        <div className="flex flex-col pb-4 w-full h-full gap-3">
             <div className="flex flex-col bg-white py-1 mt-3 px-10">
                 <div className="flex flex-row justify-between">
                     <div className="flex flex-row w-full items-center gap-2">
@@ -19,16 +19,17 @@ export default function Transactions() {
                             <img className="w-6" src="src/assets/import-icon.png" alt="Import icon" />
                             Import 
                         </button>
-                        <select name="timeFrame" id="timeFrame" className="text-primary_blue font-body font-semibold border-[3px] border-primary_blue rounded-[0.25rem] bg-transparent py-[1rem] px-2 outline-none">
-                            <option className="text-gray-600" value="week">Last 7 Days</option>
-                            <option className="text-gray-600" value="month" selected>Last 30 Days</option>
-                            <option className="text-gray-600" value="biMonth">Last 60 Days</option>
-                            <option className="text-gray-600" value="triMonth">Last 90 Days</option>
-                            <option className="text-gray-600" value="allTime">All time</option>
+                        <select name="timeFrame" id="timeFrame" className="text-primary_blue font-body font-semibold border-[3px] border-primary_blue rounded-[0.25rem] 
+                                                                            bg-transparent py-[1rem] px-2 outline-none cursor-pointer hover:bg-blue-50">
+                            <option className="text-gray-700" value="week">Last 7 Days</option>
+                            <option className="text-gray-700" value="month" selected>Last 30 Days</option>
+                            <option className="text-gray-700" value="biMonth">Last 60 Days</option>
+                            <option className="text-gray-700" value="triMonth">Last 90 Days</option>
+                            <option className="text-gray-700" value="allTime">All time</option>
                         </select>
                     </div>
                 </div>
-                <h2 className="ml-11 mt-[-0.5rem] text-md">
+                <h2 className="ml-11 mt-[-0.5rem] text-lg">
                     <span className="font-header font-semibold text-gray-700">Date range⠀-⠀</span>
                     <span className="font-header font-medium text-gray-600">18 Mar 2025⠀-⠀17 Apr 2025</span>
                 </h2>
@@ -36,17 +37,18 @@ export default function Transactions() {
             <div className="flex flex-col gap-5 bg-white py-4 px-4 ml-8 mr-8 text-black rounded-sm shadow-bb-general">
                 <div className="flex flex-row justify-between">
                     <h1 className="font-header text-xl font-bold text-gray-800 mb-2"> 24 Transactions</h1>
-                    <div className="flex flex-row gap-4 items-center">
-                         <select name="filter" id="filter" className="text-gray-500 font-header font-semibold shadow-bb-general text-md rounded-[0.2rem] bg-bb_slate py-2.5 px-3 outline-none">
-                            <option className="text-gray-600" value="latest" selected>Sort by Latest</option>
-                            <option className="text-gray-600" value="oldest">Sort by Oldest</option>
+                    <div className="flex flex-row gap-5 items-center">
+                         <select name="filter" id="filter" className="text-gray-500 font-header font-semibold shadow-bb-general text-md rounded-[0.2rem] bg-bb_slate py-2 px-3 outline-none cursor-pointer hover:bg-slate-50">
+                            <option className="text-gray-600" value="latest" selected>Sort Latest</option>
+                            <option className="text-gray-600" value="oldest">Sort Oldest</option>
                         </select>
-                        <button className="bg-primary_blue flex flex-row items-center text-white text-md font-header font-semibold gap-2 py-2.5 px-4 rounded-md">
+                        <button className="bg-primary_blue flex flex-row items-center text-white text-md font-header font-semibold gap-2 py-2 px-4 rounded-md hover:bg-btn_hover">
                             <img className="w-5" src="src/assets/plus-icon.png" alt="plus icon" />
                              Add New
                         </button>
                     </div>
                 </div>
+                <span className="absolute text-sm font-semibold font-header text-gray-500 text-left top-[10.5rem]">Select All</span>
                <Table/>
             </div>
         </div>
