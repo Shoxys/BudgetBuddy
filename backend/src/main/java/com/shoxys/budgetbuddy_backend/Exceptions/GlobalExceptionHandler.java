77ad2e273695f87f06dc0822b4b361c1914a2ca8
Jack Kong <jackadoy157@gmail.com>
2025-07-04 16:11:39 +1000

    Add custom exception handling for invalid field's for DTOs

--- backend/src/main/java/com/shoxys/budgetbuddy_backend/Exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/shoxys/budgetbuddy_backend/Exceptions/GlobalExceptionHandler.java
@@ -2,7 +2,11 @@ package com.shoxys.budgetbuddy_backend.Exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,5 +24,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleAllOthers(Exception exception) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
