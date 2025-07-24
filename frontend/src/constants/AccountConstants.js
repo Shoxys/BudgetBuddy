ed31c56ac03a8e69c6251481bb1b662c7547cb24
Jack Kong <jackadoy157@gmail.com>
2025-07-24 22:57:16 +1000

    Add class specific global constants

---
+++ frontend/src/constants/AccountConstants.js
@@ -0,0 +1,17 @@
+/**
+ * Constants for account-related configuration.
+ */
+
+export const AccountTypes = {
+  SAVINGS: 'SAVINGS',
+  INVESTMENTS: 'INVESTMENTS',
+  SPENDING: 'SPENDING',
+  GOALSAVINGS: 'GOALSAVINGS',
+};
+
+export const AccountDisplayNames = {
+  [AccountTypes.SAVINGS]: 'Saving Account',
+  [AccountTypes.INVESTMENTS]: 'Investment Account',
+  [AccountTypes.SPENDING]: 'Spending Account',
+  [AccountTypes.GOALSAVINGS]: 'Goal Savings',
+};
